53706716a005d032a1f971bdb636068713533d4e
Priyam Kumar <priyamkumar581@gmail.com>
2023-07-27 17:28:55 +0530

    Updates

---
+++ precache-manifest.2511fdebc1d1ed95554d539226ec1ec9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "af894515e32f3107f70a",
+    "url": "/weather-app/static/css/main.af1c6c85.chunk.css"
+  },
+  {
+    "revision": "af894515e32f3107f70a",
+    "url": "/weather-app/static/js/main.af894515.chunk.js"
+  },
+  {
+    "revision": "a67228bce0e664dddf29",
+    "url": "/weather-app/static/js/1.a67228bc.chunk.js"
+  },
+  {
+    "revision": "2be6f7590b391fa0dcfc",
+    "url": "/weather-app/static/js/runtime~main.2be6f759.js"
+  },
+  {
+    "revision": "9fc931d745d0285a76772712915f87c0",
+    "url": "/weather-app/static/media/normal.9fc931d7.jpg"
+  },
+  {
+    "revision": "9754f4fe019989a2f826a84df3f9fd2d",
+    "url": "/weather-app/static/media/bad.9754f4fe.jpg"
+  },
+  {
+    "revision": "7b99220ec8e215c755a2686a0d632b41",
+    "url": "/weather-app/index.html"
+  }
+];
